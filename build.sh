c535cbcd1adf041a78d270e5c25f0893800a170d
Andrzej Drabarek <andrzej.drabarek@protonmail.com>
2018-12-14 22:50:38 +0100

    Added deps: drb_graphics, imgui, glfw

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 ./compile_all_templates.sh
-clang-3.9 -g -Wall -Werror -std=c11 -I../src -fno-exceptions -ferror-limit=1 \
+clang-3.9 -g -Wall -Werror -std=c11 -I../src -I../ext/drb_graphics/include -fno-exceptions -ferror-limit=1 \
       src/main.c \
+      deps/drb_graphics/lib/libdrb_graphics.a \
+      deps/glfw-3.2.1/lib/libglfw3.a \
+      deps/imgui-1.65/imgui.a \
       -o round_table -lSDL2 -lGL -ldl -lm
