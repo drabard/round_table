d3c42b6e7947e1f55ec80f7e5c3a279adc4772a3
Andrzej Drabarek <andrzej.drabarek@protonmail.com>
2019-02-02 22:43:23 +0100

    Server receiving messages from the client and able to identify client's address.

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -23,24 +23,28 @@ static struct addrinfo* get_local_addresses(const char* port_str) {
   return result;
 }
 
-static void print_addresses(struct addrinfo* addresses) {
-  if (addresses == 0)
-    return;
-
+static void print_address(struct sockaddr* addr, socklen_t addr_len) {
   const uint32_t host_str_len = 64;
   const uint32_t serv_str_len = 64;
   char host_str[host_str_len] = {};
   char serv_str[serv_str_len] = {};
+
+  int status = getnameinfo(addr, addr_len, host_str, host_str_len, serv_str,
+                           serv_str_len, 0);
+  if (status != 0) {
+    fprintf(stderr, "ERROR: getnameinfo failed.\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("%s:%s\n", host_str, serv_str);
+}
+
+static void print_addresses(struct addrinfo* addresses) {
+  if (addresses == 0)
+    return;
+
   for (struct addrinfo* c_addr = addresses; c_addr != 0;
        c_addr = c_addr->ai_next) {
-
-    int status = getnameinfo(c_addr->ai_addr, c_addr->ai_addrlen, host_str,
-                             host_str_len, serv_str, serv_str_len, 0);
-    if (status != 0) {
-      fprintf(stderr, "ERROR: getnameinfo failed.\n");
-      exit(EXIT_FAILURE);
-    }
-    printf("%s:%s\n", host_str, serv_str);
+    print_address(c_addr->ai_addr, c_addr->ai_addrlen);
   }
 }
 
@@ -69,6 +73,23 @@ bool server_init(struct server* srv, struct addrinfo* local_addresses) {
   return true;
 }
 
+void server_update(struct server* srv) {
+  const uint32_t recv_buf_size = 128;
+  char recv_buf[recv_buf_size];
+
+  struct sockaddr src_addr;
+  socklen_t src_addr_len = sizeof(src_addr);
+
+  uint32_t nbytes = recvfrom(srv->socket, recv_buf, recv_buf_size, 0, &src_addr,
+                             &src_addr_len);
+  if (nbytes == -1)
+    return;
+
+  printf("From: ");
+  print_address(&src_addr, src_addr_len);
+  printf("[%d]: %s\n", nbytes, recv_buf);
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s port\n", argv[0]);
@@ -87,6 +108,10 @@ int main(int argc, char** argv) {
   if (!server_init(&server, local_addresses)) {
     fprintf(stderr, "ERROR: Failed to initialize server.\n");
     exit(EXIT_FAILURE);
+  }
+
+  for (;;) {
+    server_update(&server);
   }
 
   freeaddrinfo(local_addresses);
